8d55134d62b9413f3180ed87753e595731194c3d
ilya <bunamaksi@yandex.ru>
2024-02-10 18:32:12 +0500

    Add functions sum(), multiply(), divide(), subtract()

--- src/main/java/gb/jdk/Calculator.java
+++ src/main/java/gb/jdk/Calculator.java
@@ -2,5 +2,55 @@ package gb.jdk;
 
 public class Calculator {
 
+    /**
+     * Складывает два числа
+     * @param first первое число
+     * @param second второе число
+     * @param <T> тип, ограниченный Number
+     * @param <V>> тип, ограниченный Number
+     * @return double
+     */
+    public static<T extends Number, V extends Number> double sum(T first, V second) {
+        return  first.doubleValue() + second.doubleValue();
+    }
+
+    /**
+     * Перемножает два числа
+     * @param first первое число
+     * @param second второе число
+     * @param <T> тип, ограниченный Number
+     * @param <V>> тип, ограниченный Number
+     * @return double
+     */
+    public static<T extends Number, V extends Number> double multiply(T first, V second) {
+        return first.doubleValue() * second.doubleValue();
+    }
+
+    /**
+     * Делит первое число на второе
+     * @param first первое число
+     * @param second второе число
+     * @param <T> тип, ограниченный Number
+     * @param <V>> тип, ограниченный Number
+     * @return double
+     */
+    public static<T extends Number, V extends Number> double divide(T first, V second) {
+        if (second.doubleValue() == 0) {
+            throw new RuntimeException("Попытка деления на ноль");
+        }
+        return first.doubleValue() / second.doubleValue();
+    }
+
+    /**
+     * Вычитает второе число из первого
+     * @param first первое число
+     * @param second второе число
+     * @param <T> тип, ограниченный Number
+     * @param <V>> тип, ограниченный Number
+     * @return double
+     */
+    public static<T extends Number, V extends Number> double subtract(T first, V second) {
+        return first.doubleValue() - second.doubleValue();
+    }
 
 }
