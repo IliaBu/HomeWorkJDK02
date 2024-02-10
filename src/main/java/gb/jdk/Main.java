c148dc562d4b8deee97a35d717b2fe798466b484
ilya <bunamaksi@yandex.ru>
2024-02-10 20:10:24 +0500

    create code for Pair class

--- src/main/java/gb/jdk/Main.java
+++ src/main/java/gb/jdk/Main.java
@@ -1,5 +1,6 @@
 package gb.jdk;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -19,27 +20,46 @@ public class Main {
         //Task01
         int a = 7;
         float b = 9;
-        System.out.printf("Сумма чисел %d и %.2f = %.2f\n",a ,b ,Calculator.sum(a, b));
-        System.out.printf("Произведение чисел %d и %.2f = %.2f\n",a ,b ,Calculator.multiply(a, b));
-        System.out.printf("Частное от деления числа %d на %.2f = %.2f\n",a ,b ,Calculator.divide(a, b));
-        System.out.printf("Разность чисел %d и %.2f = %.2f\n",a ,b ,Calculator.subtract(a, b));
+        System.out.printf("Сумма чисел %d и %.2f = %.2f\n", a, b, Calculator.sum(a, b));
+        System.out.printf("Произведение чисел %d и %.2f = %.2f\n", a, b, Calculator.multiply(a, b));
+        System.out.printf("Частное от деления числа %d на %.2f = %.2f\n", a, b, Calculator.divide(a, b));
+        System.out.printf("Разность чисел %d и %.2f = %.2f\n", a, b, Calculator.subtract(a, b));
         System.out.println(" ");
 
         System.out.println("Проверяем одинаковые массивы:");
-        Object[] array1 = new Object[] {5, 8, 2, 9f};
-        Object[] array2 = new Object[] {3, 6, 7, 9f};
+        Object[] array1 = new Object[]{5, 8, 2, 9f};
+        Object[] array2 = new Object[]{3, 6, 7, 9f};
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
         if (Calculator.compareArrays(array1, array2))
-            System.out.println("Масссивы имеют одинаковый тип данных");
+            System.out.println("Массивы имеют одинаковый тип данных");
         System.out.println(" ");
 
         System.out.println("Проверяем неодинаковые массивы:");
-        Object[] array3 = new Object[] {0, 5, 2f, 7f};
-        Object[] array4 = new Object[] {0, 5, 2, 7f};
+        Object[] array3 = new Object[]{0, 5, 2f, 7f};
+        Object[] array4 = new Object[]{0, 5, 2, 7f};
         System.out.println(Arrays.toString(array3));
         System.out.println(Arrays.toString(array4));
         if (!Calculator.compareArrays(array3, array4))
-            System.out.println("Масссивы имеют неодинаковый тип данных");
+            System.out.println("Массивы имеют неодинаковый тип данных");
+        System.out.println(" ");
+
+        //Task02
+        System.out.println("Обобщения: ");
+        Pair<Integer, String> pair = new Pair<>(2, "9");
+        Pair<Double, String> pair1 = new Pair<>(5.5, "7");
+        Pair<Integer, Float> pair2 = new Pair<>(1, 2f);
+
+        System.out.println(pair);
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+
+        System.out.println(pair1);
+        System.out.println(pair1.getFirst());
+        System.out.println(pair1.getSecond());
+
+        System.out.println(pair2);
+        System.out.println(pair2.getFirst());
+        System.out.println(pair2.getSecond());
     }
 }
--- src/main/java/gb/jdk/Pair.java
+++ src/main/java/gb/jdk/Pair.java
@@ -1,6 +1,44 @@
 package gb.jdk;
 
-public class Pair {
+public class Pair <T,V>{
 
+    private final T first;
+    private final V second;
+
+    /**
+     * Конструктор
+     * @param first
+     * @param second
+     */
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Возвращает первый объект
+     * @return T
+     */
+    public T getFirst() {
+        return first;
+    }
+
+    /**
+     * Возвращает второй объект
+     * @return V
+     */
+    public V getSecond() {
+        return second;
+    }
+
+    /**
+     * Возвращает строковое представление объекта.
+     */
+    @Override
+    public String toString() {
+        return String.format("Первый тип: %s; второй тип: %s",
+                first.getClass().getSimpleName(),
+                second.getClass().getSimpleName());
+    }
 
 }
